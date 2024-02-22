98feca7327f3dab6d66da4351186ef8e971efbc3
Joseph Torres <jdtorres@ucdavis.edu>
2024-02-22 13:28:36 -0800

    End of Section 2

---
+++ API/Entities/AppUser.cs
@@ -0,0 +1,8 @@
+﻿namespace API.Entities;
+
+public class AppUser
+{
+    public int Id { get; set; } // Using "Id" here lets the entity framework know to treat this as primary key 
+    public string UserName { get; set; }
+
+}
