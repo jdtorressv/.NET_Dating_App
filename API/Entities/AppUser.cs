6269636112302a869ba30baaefbbe2c0cc4854cb
Joseph Torres <jdtorres@ucdavis.edu>
2024-02-24 13:52:29 -0800

    passwords, register endpt, validation

--- API/Entities/AppUser.cs
+++ API/Entities/AppUser.cs
@@ -1,8 +1,10 @@
 ﻿namespace API.Entities;
 
-public class AppUser
+public class AppUser // Entities will be converted and inserted into SQLite db via migration
 {
     public int Id { get; set; } // Using "Id" here lets the entity framework know to treat this as primary key 
     public string UserName { get; set; }
+    public byte[] passwordHash { get; set; }
+    public byte[] passwordSalt { get; set; }
 
 }
