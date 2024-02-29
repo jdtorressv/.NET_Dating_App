64bc69a90f883f96ac3ceb8dbb3ba6f15d195a9a
Joseph Torres <jdtorres@ucdavis.edu>
2024-02-29 14:37:24 -0800

    start of routing

---
+++ client/src/app/members/members-list/members-list.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-members-list',
+  templateUrl: './members-list.component.html',
+  styleUrls: ['./members-list.component.css']
+})
+export class MembersListComponent implements Onit {
+
+  ngOnInit(): void {}
+
+}
