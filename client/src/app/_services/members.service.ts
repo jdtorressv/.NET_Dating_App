fc957ab385c4d96af5d8e1ec3a393dcdb500841c
Joseph Torres <jdtorres@ucdavis.edu>
2024-03-10 12:23:39 -0700

    bugfix to users instead of user

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -11,25 +11,11 @@ export class MembersService {
   constructor(private http: HttpClient) { }
 
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users', this.getHttpOptions());
+    return this.http.get<Member[]>(this.baseUrl + 'users');  //JWT Interceptor will add token on outgoing request
   }
 
   getMember(username: string) {
-    return this.http.get<Member>(this.baseUrl + 'user/' + username, this.getHttpOptions());
-  }
-
-  getHttpOptions() {
-    const userString = localStorage.getItem('user');
-    if (!userString) {
-      console.log("No user string!");
-      return;
-    }
-    const user = JSON.parse(userString);
-    return {
-      headers: new HttpHeaders({
-        Authorization: 'Bearer ' + user.token
-      })
-    }
+    return this.http.get<Member>(this.baseUrl + 'users/' + username); //JWT Interceptor will add token on outgoing request
   }
 
 
