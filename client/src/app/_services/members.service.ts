e6cc6161bd425555c5ede89fcd158682e4ebc824
Joseph Torres <jdtorres@ucdavis.edu>
2024-03-14 13:56:05 -0700

    using service to store members state

--- client/src/app/_services/members.service.ts
+++ client/src/app/_services/members.service.ts
@@ -2,24 +2,45 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { environment } from 'src/environments/environment';
 import { Member } from '../_models/member';
+import { map, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class MembersService {
   baseUrl = environment.apiUrl;
+  members: Member[] = [];
+
   constructor(private http: HttpClient) { }
 
+  // Unlike components, services are not destroyed when not in use and can store app state
+
   getMembers() {
-    return this.http.get<Member[]>(this.baseUrl + 'users');  //JWT Interceptor will add token on outgoing request
+    // Check to see if we already have members
+    if (this.members.length > 0) return of(this.members);
+
+    return this.http.get<Member[]>(this.baseUrl + 'users').pipe(
+      map(members => {
+        this.members = members;
+        return members;
+      })
+    );  // JWT Interceptor will add token on outgoing request
   }
 
   getMember(username: string) {
+    const member = this.members.find(m => m?.userName === username);
+    if (member) return of(member);
+
     return this.http.get<Member>(this.baseUrl + 'users/' + username); //JWT Interceptor will add token on outgoing request
   }
 
   updateMember(member: Member) {
-    return this.http.put<Member>(this.baseUrl + 'users', member);
+    return this.http.put<Member>(this.baseUrl + 'users', member).pipe(
+      map(() => {
+        const index = this.members.indexOf(member);
+        this.members[index] = {...this.members[index], ...member}
+      })
+    );
 
   }
 }
