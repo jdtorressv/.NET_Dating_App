bf89eb15cd6a964d26a17c87423b74bbd79b6b9e
Joseph Torres <jdtorres@ucdavis.edu>
2024-03-19 12:55:12 -0700

    test of photo upload

--- API/Controllers/UsersController.cs
+++ API/Controllers/UsersController.cs
@@ -73,7 +73,11 @@ public class UsersController : BaseApiController
         user.Photos.Add(photo);
 
         // Save change to database
-        if (await _userRepository.SaveAllAsync()) return _mapper.Map<PhotoDTO>(photo);
+        if (await _userRepository.SaveAllAsync())
+        {
+            return CreatedAtAction(nameof(GetUser),
+             new { username = user.UserName }, _mapper.Map<PhotoDTO>(photo));
+        }
 
         return BadRequest("Problem adding photo");
     }
