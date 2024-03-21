e01268e90a28861727d862e83a0340b00fbc92b0
Joseph Torres <jdtorres@ucdavis.edu>
2024-03-20 21:08:06 -0700

    delete photo

--- client/src/app/members/photo-editor/photo-editor.component.ts
+++ client/src/app/members/photo-editor/photo-editor.component.ts
@@ -45,8 +45,18 @@ export class PhotoEditorComponent implements OnInit {
           this.member.photoUrl = photo.url;
           this.member.photos.forEach(p => {
             if (p.isMain) p.isMain = false;
-            if (p.id == photo.id) p.isMain = true; 
+            if (p.id == photo.id) p.isMain = true;
           })
+        }
+      }
+    })
+  }
+
+  deletePhoto(photoId: number) {
+    this.memberService.deletePhoto(photoId).subscribe({
+      next: () => {
+        if (this.member) {
+          this.member.photos = this.member.photos.filter(x => x.id != photoId);
         }
       }
     })
