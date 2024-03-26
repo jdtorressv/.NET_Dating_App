e95e6de5069226421779743bde93bbe291ffbbf0
Joseph Torres <jdtorres@ucdavis.edu>
2024-03-26 09:30:52 -0700

    create LikesRepository

---
+++ API/Interfaces/ILikesRepository.cs
@@ -0,0 +1,13 @@
+﻿using API.DTOs;
+using API.Entities;
+
+namespace API.Interfaces;
+
+public interface ILikesRepository
+{
+    Task<UserLike> GetUserLike(int sourceUserId, int targetUserId);
+    Task<AppUser> GetUserWithLikes(int userId);
+
+    // Predicate refers to "likes" or "liked by"
+    Task<IEnumerable<LikeDTO>> GetUserLikes(string predicate, int userId);
+}
