5309f731a1719865213584922b47d3cf4018f569
Joseph Torres <jdtorres@ucdavis.edu>
2024-03-27 08:43:34 -0700

    Message to MessageDTO automapper, CreateMessageDTO

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -10,10 +10,16 @@ public class AutoMapperProfiles : Profile
     public AutoMapperProfiles()
     {
         CreateMap<AppUser, MemberDTO>() // Map AppUser into MemberDTO
-            .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(x => x.IsMain).Url))
+            .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos
+                .FirstOrDefault(x => x.IsMain).Url))
             .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOfBirth.CalculateAge()));
         CreateMap<Photo, PhotoDTO>(); // Map Photo into PhotoDTO
         CreateMap<MemberUpdateDTO, AppUser>(); // Map member updates to App User
         CreateMap<RegisterDTO, AppUser>(); // Maps RegisterDTO details to App User
+        CreateMap<Message, MessageDTO>() // Maps Message to MessageDTO
+            .ForMember(dest => dest.SenderPhotoUrl, o => o.MapFrom(s => s.Sender.Photos
+                .FirstOrDefault(x => x.IsMain).Url))
+            .ForMember(dest => dest.RecipientPhotoUrl, o => o.MapFrom(s => s.Recipient.Photos
+                .FirstOrDefault(x => x.IsMain).Url));
     }
 }
