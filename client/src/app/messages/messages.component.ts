4aed599a1fdb712c5f7eb17a3746d2a92120e6c1
Joseph Torres <jdtorres@ucdavis.edu>
2024-03-28 09:59:42 -0700

    delete bugfix

--- client/src/app/messages/messages.component.ts
+++ client/src/app/messages/messages.component.ts
@@ -37,7 +37,10 @@ export class MessagesComponent implements OnInit {
 
   deleteMessage(id: number) {
     this.messageService.deleteMessage(id).subscribe({
-      next: () => this.messages?.splice(this.messages.findIndex(m => m.id === id, 1))
+      next: () => {
+        this.messages?.splice(this.messages.findIndex(m => m.id === id, 1));
+        this.loadMessages();
+      }
     })
   }
 
