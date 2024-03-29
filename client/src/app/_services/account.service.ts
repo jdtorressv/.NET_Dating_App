af5068d3f93efd5521b9f942efbb6210c0b8a97c
Joseph Torres <jdtorres@ucdavis.edu>
2024-03-28 19:04:37 -0700

    admin guard

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -34,6 +34,10 @@ export class AccountService {
   }
 
   setCurrentUser(user: User) {
+    user.roles = []
+    const roles = this.getDecodedToken(user.token).role;
+    Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
+    
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
   }
@@ -41,6 +45,11 @@ export class AccountService {
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+  }
+
+  // Returns part of token with roles arr in it; if only one role, comes back as single string
+  getDecodedToken(token: string) {
+    return JSON.parse(atob(token.split('.')[1]));
   }
 
 }
