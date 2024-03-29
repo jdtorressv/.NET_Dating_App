532926f3270b92cbab7b484334342e2b2ec26f88
Joseph Torres <jdtorres@ucdavis.edu>
2024-03-29 10:42:16 -0700

    login presence notification client side

--- client/src/app/_services/account.service.ts
+++ client/src/app/_services/account.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, map } from 'rxjs';
 import { User } from '../_models/user';
 import { environment } from 'src/environments/environment';
+import { PresenceService } from './presence.service';
 
 @Injectable({
   providedIn: 'root'
@@ -12,7 +13,7 @@ export class AccountService {
   private currentUserSource = new BehaviorSubject<User | null>(null);
   currentUser$ = this.currentUserSource.asObservable();
 
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient, private presenceService: PresenceService) {}
 
   login(model: any) {
     console.log(this.baseUrl);
@@ -37,14 +38,16 @@ export class AccountService {
     user.roles = []
     const roles = this.getDecodedToken(user.token).role;
     Array.isArray(roles) ? user.roles = roles : user.roles.push(roles);
-    
+
     localStorage.setItem('user', JSON.stringify(user));
     this.currentUserSource.next(user);
+    this.presenceService.createHubConnection(user);
   }
 
   logout() {
     localStorage.removeItem('user');
     this.currentUserSource.next(null);
+    this.presenceService.stopHubConnection();
   }
 
   // Returns part of token with roles arr in it; if only one role, comes back as single string
