a6e35e1625f2e60ed2cf1b1e476dba648765121b
Joseph Torres <jdtorres@ucdavis.edu>
2024-03-28 21:41:32 -0700

    edit roles cont

--- client/src/app/admin/user-management/user-management.component.ts
+++ client/src/app/admin/user-management/user-management.component.ts
@@ -40,7 +40,6 @@ export class UserManagementComponent implements OnInit {
       }
     }
     this.bsModalRef = this.modalService.show(RolesModalComponent, initialState);
-    this.bsModalRef.content!.closeBtnName = 'Close';
   }
 
 }
