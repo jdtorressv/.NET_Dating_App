ff09ac513d79f8dc08ebf59a7f9a37ab16128c5d
Joseph Torres <jdtorres@ucdavis.edu>
2024-04-01 19:17:27 -0700

    fix UTC date format

--- API/Helpers/AutoMapperProfiles.cs
+++ API/Helpers/AutoMapperProfiles.cs
@@ -21,5 +21,9 @@ public class AutoMapperProfiles : Profile
                 .FirstOrDefault(x => x.IsMain).Url))
             .ForMember(dest => dest.RecipientPhotoUrl, o => o.MapFrom(s => s.Recipient.Photos
                 .FirstOrDefault(x => x.IsMain).Url));
+        CreateMap<DateTime, DateTime>().ConvertUsing(d => DateTime.SpecifyKind(d, DateTimeKind.Utc));
+        CreateMap<DateTime?, DateTime?>().ConvertUsing(d => d.HasValue ?
+            DateTime.SpecifyKind(d.Value, DateTimeKind.Utc) : null);
+
     }
 }
